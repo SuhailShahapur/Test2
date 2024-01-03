0a7cf72d522b877ae9dafcd5120b8f80f6c12619
suhail.shahapur <suhail.shahapur15@gmail.com>
2024-01-04 01:21:00 +0530

    add is performed in the program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 1;
+        int b =5;
+        int result = a+b;
+        System.out.println(result);
     }
 }
