fe8a8147914f871836cc82fdc045ccdc0137ddaa
suhail.shahapur <suhail.shahapur15@gmail.com>
2024-01-10 00:00:56 +0530

    for each progam

---
+++ src/testfor1.java
@@ -0,0 +1,9 @@
+public class testfor1 {
+    public static void main(String[] args) {
+        int[] a ={1,2,3,4,5};
+        for (int j : a) {
+            System.out.println(j);
+        }
+
+    }
+}
